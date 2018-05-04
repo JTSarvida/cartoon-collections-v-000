5216f8fd7e5903b8388d4c974a3bf69c042ae835
Joshua Tan <jtsarvida@gmail.com>
2018-05-04 14:26:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,18 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array_of_dwarves)# code an argument here
   # Your code here
+  array_of_dwarves.each_with_index do |dwarf, index|
+    puts "#{index += 1}#{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  capt = planeteer_calls.collect do |elements|
+    elements.capitalize
+  end
+  capt.collect do |elements|
+    elements + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
